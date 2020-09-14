dba3c355ad8383d007125da51b665665a6f71a22
Juan Manuel Torsello <jtorsello@dc.uba.ar>
2020-09-14 14:22:13 -0300

    tp1 orga2

---
+++ tp1-bundle.v0.1/entregable/tp1-bundle.v0.1/solucion/lib.asm
@@ -0,0 +1,67 @@
+
+section .data
+
+section .text
+
+global floatCmp
+global floatClone
+global floatDelete
+global floatPrint
+
+global strClone
+global strLen
+global strCmp
+global strDelete
+global strPrint
+
+global docClone
+global docDelete
+
+global listAdd
+
+global treeInsert
+global treePrint
+
+;*** Float ***
+
+floatCmp:
+ret
+floatClone:
+ret
+floatDelete:
+ret
+floatPrint:
+ret
+
+;*** String ***
+
+strClone:
+ret
+strLen:
+ret
+strCmp:
+ret
+strDelete:
+ret
+strPrint:
+ret
+
+;*** Document ***
+
+docClone:
+ret
+docDelete:
+ret
+
+;*** List ***
+
+listAdd:
+ret
+
+;*** Tree ***
+
+treeInsert:
+ret
+treePrint:
+ret
+
